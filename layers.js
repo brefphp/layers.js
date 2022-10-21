04ebc5c10f48d6be13fcfd65ae03b0ea46fc156b
Matthieu Napoli <matthieu@mnapoli.fr>
2022-10-21 14:54:44 +0200

    Add ARM layers

--- layers.js
+++ layers.js
@@ -5,10 +5,14 @@ const layerVersions = require('./layers.json');
  * Returns the ARN for the function layer.
  * @param {string} region
  * @param {string} phpVersion (e.g. '8.1')
+ * @param {'x86'|'arm'} platform
  * @returns {string} Layer ARN
  */
-function functionLayerArn(region, phpVersion) {
-    const layerName = 'php-' + phpVersion.replace('.', '');
+function functionLayerArn(region, phpVersion, platform) {
+    let layerName = 'php-' + phpVersion.replace('.', '');
+    if (platform === 'arm') {
+        layerName = 'arm-' + layerName;
+    }
     const version = layerVersions[layerName][region];
     return `arn:aws:lambda:${region}:534081306603:layer:${layerName}:${version}`;
 }
@@ -17,10 +21,14 @@ function functionLayerArn(region, phpVersion) {
  * Returns the ARN for the FPM layer.
  * @param {string} region
  * @param {string} phpVersion (e.g. '8.1')
+ * @param {'x86'|'arm'} platform
  * @returns {string} Layer ARN
  */
-function fpmLayerArn(region, phpVersion) {
-    const layerName = 'php-' + phpVersion.replace('.', '') + '-fpm';
+function fpmLayerArn(region, phpVersion, platform) {
+    let layerName = 'php-' + phpVersion.replace('.', '') + '-fpm';
+    if (platform === 'arm') {
+        layerName = 'arm-' + layerName;
+    }
     const version = layerVersions[layerName][region];
     return `arn:aws:lambda:${region}:534081306603:layer:${layerName}:${version}`;
 }
@@ -37,7 +45,7 @@ function consoleLayerArn(region) {
 
 module.exports = {
     // Expose the JSON data as a JS dependency for easier use in programmatic environments
-    layerVersions: layerVersions,
+    layerVersions,
     functionLayerArn,
     fpmLayerArn,
     consoleLayerArn,
